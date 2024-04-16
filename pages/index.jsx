14358de77b71db310323878c18dd4935d2314833
AdAntonio07 <antonio.ferreira1320@gmail.com>
2024-04-16 18:25:36 -0300

    Melhorado Index

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,12 @@ import styled from "styled-components";
 import ImgDeFundoComChildren from "../src/components/imgDeFundoComChildren/imgDeFundoComChildren";
 
 const StyledDiv = styled.div`
-  background-color: red;
+  background-color: ${(props) => props.theme.colors.white};
+  min-width: 500px;
+
+  @media (max-width: 500px) {
+    min-width: 100%;
+  }
 `;
 
 export default function HomePage() {
