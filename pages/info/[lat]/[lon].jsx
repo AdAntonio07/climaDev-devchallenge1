5b6757e8094fde410886ba3a8b21233e836f9284
AdAntonio07 <antonio.ferreira1320@gmail.com>
2024-04-22 17:51:15 -0300

    Criado espaço na pagina de informações

--- pages/info/[lat]/[lon].jsx
+++ pages/info/[lat]/[lon].jsx
@@ -7,13 +7,31 @@ const Body = styled.div`
   background-color: ${(props) => props.theme.colors.grey};
   min-height: 100vh;
 `;
+const Conteudo = styled.div`
+  padding: 80px 0;
+`;
+const Texto = styled.p`
+  font-size: 20px;
+  padding: 0;
+  margin: 0;
+`;
+const NomeDaCidade = styled.h2`
+  font-size: 60px;
+  padding: 0;
+  margin: 0;
+`;
 
 export default function InfoPage() {
   return (
     <>
       <Navbar />
       <Body>
-        <Container>Conteúdo</Container>
+        <Container>
+          <Conteudo>
+            <Texto>Previsão do tempo para</Texto>
+            <NomeDaCidade>Seara, SC, Brasil</NomeDaCidade>
+          </Conteudo>
+        </Container>
       </Body>
     </>
   );
